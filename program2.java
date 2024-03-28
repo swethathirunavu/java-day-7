76431094a9195705b688694072deb0fa33baddb7
Swetha T <145834012+swethathirunavu@users.noreply.github.com>
2024-03-28 17:01:00 +0530

    Create program2.java

---
+++ program2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        greet("Your Name");
+    }
+
+    static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
