cb938fba5879b5b8ce1bf177d99ac3bd1d9fd9fc
Swetha T <145834012+swethathirunavu@users.noreply.github.com>
2024-03-28 16:51:01 +0530

    Update and rename README.md to program1.java

---
+++ program1.java
@@ -0,0 +1,11 @@
+# java-day-7
+public class Main {
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("Result: " + result);
+    }
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+}
